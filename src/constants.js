a966043abf8994801204ec99e902dfb3a490c4b1
Oliver Benns <oliverbenns@gmail.com>
2017-05-07 01:23:52 +0900

    Add color constants

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const palette = {
+  primary: {
+    white: '#ffffff',
+    lightCyan: '#55ffff',
+    lightMagenta: '#ff55ff',
+    black: '#000000',
+  },
+  secondary: {
+    yellow: '#ffff55',
+    lightGreen: '#55ff55',
+    lightRed: '#ff5555',
+    black: '#000000',
+  },
+};
