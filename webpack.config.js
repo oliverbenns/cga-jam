f66310d7ea14e276d92fa0ef44da7dda01352986
Oliver Benns <oliverbenns@gmail.com>
2017-05-07 00:17:56 +0900

    Add Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const APP_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const NODE_ENV = process.env.NODE_ENV;
+
+const config = {
+  entry: `${APP_DIR}/index.js`,
+  output: {
+    path: BUILD_DIR,
+    filename: 'assets/js/bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    modules: [APP_DIR, 'node_modules'],
+    alias: {
+      // https://github.com/webpack/webpack/issues/4666
+      constants: `${APP_DIR}/constants`,
+    },
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+        include: APP_DIR,
+      },
+    ],
+  },
+  plugins: NODE_ENV === 'production' ? [ new webpack.optimize.UglifyJsPlugin() ] : [],
+  devServer: {
+    contentBase: BUILD_DIR,
+    port: 8080,
+    stats: 'minimal',
+  },
+};
+
+module.exports = config;
