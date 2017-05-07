43550f7e7641c9471616ca321bcaabfc7a3b591c
Oliver Benns <oliverbenns@gmail.com>
2017-05-07 10:08:09 +0900

    Add Phaser and expose to global window obj

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,9 @@ const path = require('path');
 
 const APP_DIR = path.resolve(__dirname, 'src');
 const BUILD_DIR = path.resolve(__dirname, 'public');
+const PHASER_DIR = path.join(__dirname, '/node_modules/phaser/');
 const NODE_ENV = process.env.NODE_ENV;
+
 
 const config = {
   entry: `${APP_DIR}/index.js`,
@@ -17,6 +19,9 @@ const config = {
     alias: {
       // https://github.com/webpack/webpack/issues/4666
       constants: `${APP_DIR}/constants`,
+      phaser: path.join(PHASER_DIR, 'build/custom/phaser-split.js'),
+      pixi: path.join(PHASER_DIR, 'build/custom/pixi.js'),
+      p2: path.join(PHASER_DIR, 'build/custom/p2.js'),
     },
   },
   devtool: 'source-map',
@@ -27,6 +32,28 @@ const config = {
         exclude: /node_modules/,
         use: ['babel-loader'],
         include: APP_DIR,
+      },
+      // https://github.com/photonstorm/phaser/issues/2762
+      {
+        test: /pixi\.js/,
+        use: [{
+          loader: 'expose-loader',
+          options: 'PIXI',
+        }],
+      },
+      {
+        test: /phaser-split\.js$/,
+        use: [{
+          loader: 'expose-loader',
+          options: 'Phaser',
+        }],
+      },
+      {
+        test: /p2\.js/,
+        use: [{
+          loader: 'expose-loader',
+          options: 'p2',
+        }],
       },
     ],
   },
