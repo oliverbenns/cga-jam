cec6abbf9d8bf0195af70cc736b5e7653cf6e41e
Oliver Benns <oliverbenns@gmail.com>
2017-05-07 11:32:01 +0900

    Initialise phaser and print color palettes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-const canvas = document.getElementById('canvas');
-const context = canvas.getContext('2d');
+import 'pixi';
+import 'p2';
+import 'phaser';
 
-context.font = '30px Arial';
-context.fillText('Hello World', 10, 50);
+import { palette } from 'constants';
+
+const game = new Phaser.Game(1024, 768, Phaser.AUTO, '', { create });
+
+const createPaletteSquares = (palette, y = 0) => {
+  const height = game.height * 0.5;
+  const width = game.width * 0.25;
+
+  return Object
+    .values(palette)
+    .map((hex, index) => ({
+      hex,
+      rect: new Phaser.Rectangle(index * width, y, width, height),
+    }))
+};
+
+function create() {
+  const primary = createPaletteSquares(palette.primary);
+  const secondary = createPaletteSquares(palette.secondary, game.world.centerY);
+
+  primary.concat(secondary).forEach(square => game.debug.geom(square.rect, square.hex));
+};
