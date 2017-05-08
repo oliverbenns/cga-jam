28284f8c571078a8174ece1d27c95ae47d53821b
Oliver Benns <oliverbenns@gmail.com>
2017-05-08 13:23:02 +0900

    Create game world and player

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'phaser';
 import { ASSETS, PALETTE } from 'constants';
 
 // Original CGA used 320 x 200, so aspect ratio is 8:5
-const game = new Phaser.Game(960, 600, Phaser.AUTO, '', { create, preload });
+const game = new Phaser.Game(960, 600, Phaser.AUTO, '', { create, preload, update });
 
 const createPaletteSquares = (palette, y = 0) => {
   const height = game.height * 0.5;
@@ -23,12 +23,72 @@ function preload() {
   game.load.image(ASSETS.BACKGROUND, 'assets/img/background.png');
   game.load.image(ASSETS.PLATFORM, 'assets/img/platform.png');
   game.load.image(ASSETS.PICKUP, 'assets/img/pickup.png');
-  game.load.spritesheet(ASSETS.PLAYER, 'assets/img/player.png', 0, 0);
+  game.load.spritesheet(ASSETS.PLAYER, 'assets/img/player.png', 20, 32);
 }
+
+let platforms;
+let player;
 
 function create() {
   const primary = createPaletteSquares(PALETTE.PRIMARY);
   const secondary = createPaletteSquares(PALETTE.SECONDARY, game.world.centerY);
 
-  primary.concat(secondary).forEach(square => game.debug.geom(square.rect, square.hex));
+  // primary.concat(secondary).forEach(square => game.debug.geom(square.rect, square.hex));
+
+  //
+  // GAME WORLD
+  //
+
+  // We're going to be using physics, so enable the Arcade Physics system
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+
+  // A simple background for our game
+  game.add.sprite(0, 0, ASSETS.BACKGROUND);
+
+  // The platforms group contains the ground and the 2 ledges we can jump on
+  platforms = game.add.group();
+
+  // We will enable physics for any object that is created in this group
+  platforms.enableBody = true;
+
+  // Here we create the ground.
+  const ground = platforms.create(0, game.world.height - 20, ASSETS.PLATFORM);
+
+  // Scale it to fit the width of the game (the original sprite is 400x32 in size)
+  ground.scale.setTo(2, 1);
+
+  // This stops it from falling away when you jump on it
+  ground.body.immovable = true;
+
+  // Now let's create two ledges
+  const leftPlatform = platforms.create(0, game.world.centerY, ASSETS.PLATFORM);
+  leftPlatform.body.immovable = true;
+
+  const rightPlatform = platforms.create(game.world.centerX, game.world.centerY * 1.5, ASSETS.PLATFORM);
+  rightPlatform.body.immovable = true;
+
+  //
+  // PLAYER
+  //
+
+  // The player and its settings
+  player = game.add.sprite(game.world.centerX - 10, game.world.centerY, ASSETS.PLAYER, 1);
+
+  // We need to enable physics on the player
+  game.physics.arcade.enable(player);
+
+  // Player physics properties. Give the little guy a slight bounce.
+  player.body.bounce.y = 0.2;
+  player.body.gravity.y = 300;
+  player.body.collideWorldBounds = true;
+
+  // Our two animations, walking left and right.
+  player.animations.add('left', [0, 1], 10, true);
+  player.animations.add('right', [2, 1], 10, true);
 };
+
+
+function update() {
+  //  Collide the player and the stars with the platforms
+  game.physics.arcade.collide(player, platforms);
+}
