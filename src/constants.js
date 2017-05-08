ca5ecb36ecde9de7322cff7055158cb501a23414
Oliver Benns <oliverbenns@gmail.com>
2017-05-08 11:54:19 +0900

    Update constants to be uppercase

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,14 @@
-export const palette = {
-  primary: {
-    black: '#000000',
-    white: '#ffffff',
-    lightCyan: '#55ffff',
-    lightMagenta: '#ff55ff',
+export const PALETTE = {
+  PRIMARY: {
+    BLACK: '#000000',
+    WHITE: '#ffffff',
+    LIGHT_CYAN: '#55ffff',
+    LIGHT_MAGENTA: '#ff55ff',
   },
-  secondary: {
-    yellow: '#ffff55',
-    lightGreen: '#55ff55',
-    lightRed: '#ff5555',
-    black: '#000000',
+  SECONDARY: {
+    YELLOW: '#ffff55',
+    LIGHT_GREEN: '#55ff55',
+    LIGHT_RED: '#ff5555',
+    BLACK: '#000000',
   },
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'pixi';
 import 'p2';
 import 'phaser';
 
-import { palette } from 'constants';
+import { PALETTE } from 'constants';
 
 // Original CGA used 320 x 200, so aspect ratio is 8:5
 const game = new Phaser.Game(960, 600, Phaser.AUTO, '', { create });
@@ -19,9 +19,16 @@ const createPaletteSquares = (palette, y = 0) => {
     }))
 };
 
+function preload() {
+  game.load.image('sky', 'assets/sky.png');
+  game.load.image('ground', 'assets/platform.png');
+  game.load.image('star', 'assets/star.png');
+  game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+}
+
 function create() {
-  const primary = createPaletteSquares(palette.primary);
-  const secondary = createPaletteSquares(palette.secondary, game.world.centerY);
+  const primary = createPaletteSquares(PALETTE.PRIMARY);
+  const secondary = createPaletteSquares(PALETTE.SECONDARY, game.world.centerY);
 
   primary.concat(secondary).forEach(square => game.debug.geom(square.rect, square.hex));
 };
