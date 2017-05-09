9abf323b90b6f79ef1a87fede21852490f811027
Oliver Benns <oliverbenns@gmail.com>
2017-05-09 18:03:19 +0900

    Move player movement to game object

--- src/index.js
+++ src/index.js
@@ -109,43 +109,9 @@ function create() {
 
 
 function update() {
-  //
-  // PLAYER MOVEMENT
-  //
-
-  const hitPlatform = game.physics.arcade.collide(player, platforms);
   game.physics.arcade.collide(pickups, platforms);
 
   const collectPickup = (player, star) => star.kill();
 
   game.physics.arcade.overlap(player, pickups, collectPickup, null, this);
-
-  // Reset the players velocity (movement)
-  player.body.velocity.x = 0;
-  // Centre anchor to allow for flipping
-  player.anchor.setTo(.5,.5);
-
-  if (cursors.left.isDown) {
-    // Move to the left
-    player.body.velocity.x = -150;
-    player.scale.x = -1;
-    player.animations.play('left');
-
-  } else if (cursors.right.isDown) {
-    // Move to the right
-    player.body.velocity.x = 150;
-    player.scale.x = 1;
-
-    player.animations.play('right');
-  } else {
-    // Stand still
-    player.animations.stop();
-
-    player.frame = 1;
-  }
-
-  //  Allow the player to jump if they are touching the ground.
-  if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
-    player.body.velocity.y = -300;
-  }
 }
