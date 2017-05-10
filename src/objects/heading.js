b67bfad7c9151587a4d221be888bcef97d91279c
Oliver Benns <oliverbenns@gmail.com>
2017-05-10 15:23:02 +0900

    Add heading object

---
+++ src/objects/heading.js
@@ -0,0 +1,11 @@
+import { ASSETS, PALETTE } from 'constants';
+
+export default class Heading extends Phaser.Text {
+  constructor(game, text) {
+    const { world } = game;
+
+    super(game, world.centerX, world.centerY * 0.75, text, { font: '96px Arial', fill: PALETTE.PRIMARY.LIGHT_CYAN })
+
+    this.anchor.setTo(0.5, 0.5);
+  }
+}
--- src/states/title.js
+++ src/states/title.js
@@ -1,11 +1,17 @@
 import { ASSETS, STATES } from 'constants';
 
 import Button from 'objects/button';
+import Heading from 'objects/heading';
 
 export default class Title extends Phaser.State {
   create() {
-    const button = new Button(this.game, this.handleClick, 'Start Game');
-    this.game.add.existing(button);
+    const { game } = this;
+    const objects = [
+      new Heading(game, 'CGA Jam'),
+      new Button(game, this.handleClick, 'Start game'),
+    ];
+
+    objects.forEach(game.add.existing, this);
   }
 
   handleClick() {
