7812280609e0c34d7c66f52de1b6d26889c67c4f
Oliver Benns <oliverbenns@gmail.com>
2017-06-02 16:28:54 +0900

    Minor fix

--- src/states/boot.js
+++ src/states/boot.js
@@ -4,13 +4,11 @@ export default class Boot extends Phaser.State {
   preload() {
     const { load } = this.game;
 
-
-
     load.image(ASSETS.BACKGROUND, 'assets/img/background.png');
-    load.spritesheet(ASSETS.PLAYER, 'assets/img/player.png', 20, 32);
-    load.spritesheet(ASSETS.BUTTON, 'assets/img/button.png', 190, 49);
-    load.spritesheet(ASSETS.TARGET, 'assets/img/target.png', 80, 80);
     load.spritesheet(ASSETS.BLOCK, 'assets/img/block.png', 40, 40);
+    load.spritesheet(ASSETS.BUTTON, 'assets/img/button.png', 190, 49);
+    load.spritesheet(ASSETS.PLAYER, 'assets/img/player.png', 20, 32);
+    load.spritesheet(ASSETS.TARGET, 'assets/img/target.png', 80, 80);
   }
 
   create() {
