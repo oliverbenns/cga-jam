735052be8a4af62084a60f4f7cb23b86298bd8c2
Oliver Benns <oliverbenns@gmail.com>
2017-06-07 15:42:48 +0900

    Preload material

--- src/states/level.js
+++ src/states/level.js
@@ -17,6 +17,22 @@ export default class Level extends Phaser.State {
     this.number = number;
   }
 
+  preload() {
+    const bulletMaterial = getMaterial(this.game, MATERIALS.BULLET);
+    const targetMaterial = getMaterial(this.game, MATERIALS.TARGET);
+
+    // @TODO: Why can't I add this to states/boot preload?
+    this.game.physics.p2.createContactMaterial(bulletMaterial, targetMaterial, {
+      friction: 1,     // Friction to use in the contact of these two materials.
+      restitution: 1,  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
+      stiffness: 1e7,    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
+      relaxation: 3,     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
+      frictionStiffness: 1e7,    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
+      frictionRelaxation: 3,     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
+      surfaceVelocity: 0,        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
+    });
+  }
+
   create() {
     const { data, game } = this;
 
@@ -44,22 +60,6 @@ export default class Level extends Phaser.State {
 
     const border = new Border(game, [playerPosition]);
     game.add.existing(border);
-
-    const bulletMaterial = getMaterial(game, MATERIALS.BULLET);
-    const targetMaterial = getMaterial(game, MATERIALS.TARGET);
-
-    //  Here is the contact material. It's a combination of 2 materials, so whenever shapes with
-    //  those 2 materials collide it uses the following settings.
-    //  A single material can be used by as many different sprites as you like.
-    var contactMaterial = game.physics.p2.createContactMaterial(bulletMaterial, targetMaterial);
-
-    contactMaterial.friction = 1;     // Friction to use in the contact of these two materials.
-    contactMaterial.restitution = 1.0;  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
-    contactMaterial.stiffness = 1e7;    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
-    contactMaterial.relaxation = 3;     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
-    contactMaterial.frictionStiffness = 1e7;    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
-    contactMaterial.frictionRelaxation = 3;     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
-    contactMaterial.surfaceVelocity = 0;        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
   };
 
   endGame() {
