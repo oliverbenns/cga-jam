6af8395c2d8228de16e512e58b026bd731ffcb85
Oliver Benns <oliverbenns@gmail.com>
2017-06-07 13:27:34 +0900

    Fix cell positioning

--- src/grid.js
+++ src/grid.js
@@ -1,9 +1,5 @@
 import { CANVAS } from 'constants';
 
 export const cell = val => {
-  return val * CANVAS.CELL_SIZE;
-};
-
-export const outofRange = val => {
-  return val * CANVAS.CELL_SIZE;
+  return val * CANVAS.CELL_SIZE + CANVAS.CELL_SIZE / 2;
 };
--- src/objects/block.js
+++ src/objects/block.js
@@ -10,11 +10,6 @@ export default class Block extends Phaser.Sprite {
     this.body.setRectangleFromSprite(this);
     this.body.static = true;
 
-    // http://www.html5gamedevs.com/topic/8686-enabling-p2-physics-moves-the-sprite-to-the-left-and-top-exactly-half-the-sprite-size/
-    // @TODO: I think I need to define the grid cells to be off centre axis now.
-    this.body.x += this.width / 2;
-    this.body.y += this.height / 2;
-
     game.debug.body(this);
   }
 }
