0079c31fbb35b7d037e7638764139133a6e02f32
Oliver Benns <oliverbenns@gmail.com>
2017-06-09 16:28:00 +0900

    Export materials as separate config

--- src/states/level.js
+++ src/states/level.js
@@ -8,6 +8,7 @@ import Enemy from 'objects/enemy';
 import Target from 'objects/target';
 import { cell } from 'grid';
 import { createCollisionGroup, getMaterial } from 'utils';
+import { bounce } from 'materials';
 
 import Bullet from 'objects/bullet';
 
@@ -24,15 +25,7 @@ export default class Level extends Phaser.State {
     const targetMaterial = getMaterial(this.game, MATERIALS.TARGET);
 
     // @TODO: Why can't I add this to states/boot preload?
-    this.game.physics.p2.createContactMaterial(bulletMaterial, targetMaterial, {
-      friction: 0.3,     // Friction to use in the contact of these two materials.
-      restitution: 1,  // Restitution (i.e. how bouncy it is!) to use in the contact of these two materials.
-      stiffness: 1e7,    // Stiffness of the resulting ContactEquation that this ContactMaterial generate.
-      relaxation: 3,     // Relaxation of the resulting ContactEquation that this ContactMaterial generate.
-      frictionStiffness: 1e7,    // Stiffness of the resulting FrictionEquation that this ContactMaterial generate.
-      frictionRelaxation: 3,     // Relaxation of the resulting FrictionEquation that this ContactMaterial generate.
-      surfaceVelocity: 0,        // Will add surface velocity to this material. If bodyA rests on top if bodyB, and the surface velocity is positive, bodyA will slide to the right.
-    });
+    this.game.physics.p2.createContactMaterial(bulletMaterial, targetMaterial, bounce);
 
     // Docs say these params are true by default. But without these it doesn't work.
     this.game.physics.p2.setBoundsToWorld(true, true, true, true, true);
